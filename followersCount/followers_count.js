912b72fe32965911bffc8e1fe605fc047dbba518
Hussam-Alkhader <hussam.alkhader@gmail.com>
2024-07-22 13:16:07 +0300

    Update followers_count.js

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -9,15 +9,23 @@ function increaseCount() {
 
 
 function checkCountValue() {
-    if (count === 10) {
-      alert("Your Instagram post gained 10 followers! Congratulations!");
-    } else if (count === 20) {
-      alert("Your Instagram post gained 20 followers! Keep it up!");
-    }
-  }
+    const messages = {
+        10: "Your Instagram post gained 10 followers! Congratulations!",
+        20: "Your Instagram post gained 20 followers! Keep it up!"
+      };
+  
+      if (messages[count]) {
+        alert(messages[count]);
+      }
+}
 
 
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
 
+function resetCount(){
+    count = 0;
+    alert("Followers count has been reset");
+    displayCount();
+}   
