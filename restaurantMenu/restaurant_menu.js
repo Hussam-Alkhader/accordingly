29f30eace433b4bf020c5c8a6e38fd6381dee8e6
Hussam-Alkhader <hussam@gmail.com>
2024-07-23 06:07:42 -0400

    code for restaurant menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,17 @@
+const breakfastMenu = {'Pancakes':12, 'Eggs Benedict':22.99, 'Oatmeal':21.99, 'Frittata':15};
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+
+const breakfastMenuItemsHTML = Object.entries(breakfastMenu).map(([item, price], index) => `<p>Item ${index + 1}: ${item}- $${price}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+
+const dessertMenuItemsHTML = dessertMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('dessertMenuItems').innerHTML = dessertMenuItemsHTML;
